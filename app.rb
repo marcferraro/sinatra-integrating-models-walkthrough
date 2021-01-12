4db8e8ff67e6b810710d943fc682ef160d348327
Marc <marc.ferraro1@gmail.com>
2021-01-11 23:07:09 -0600

    commit tree 87f67d8470976a0a5c6d6146403ac349d1da7de6

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
+require 'pry'
+
 
 class App < Sinatra::Base
   get '/' do
@@ -7,8 +9,7 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
-
+    @analyzedtext = TextAnalyzer.new(params[:user_text])
     erb :results
   end
 end
